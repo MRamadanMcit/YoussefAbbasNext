dd36dfb0cc28296bf87ae9d8bad7489b6ae6c5da
MRamadanMcit <mramadan.mcit@gmail.com>
2024-12-24 13:07:14 +0200

    Add Routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Homepage() {
-  return (<div>
+  return (
+    <div>
       <h1>Hello Homepage</h1>
-      <div>Welcome ro next Js</div>
-  </div>    
-  )
+      <div>Welcome to next Js</div>
+      <div>
+        <Link href="/about">About</Link>
+        <Link href="/admin">Admin</Link>
+        <Link href="/articles">Articles</Link>
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+      </div>
+    </div>
+  );
 }
