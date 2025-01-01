c129642d786a1ee3a0aa1aa4e4f68b9ae09eb5bf
MRamadanMcit <mramadan.mcit@gmail.com>
2025-01-01 16:21:50 +0200

    Differance between server and client component

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,9 +1,16 @@
-import Link from "next/link";
-import { GrTechnology } from "react-icons/gr";
+"use client";
 
-import styles from './header.module.css';
+import Link from "next/link";
+import { useState } from "react";
+import { GrTechnology } from "react-icons/gr";
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoCloseOutline } from "react-icons/io5";
+
+import styles from "./header.module.css";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className={styles.navbar}>
       <div>
@@ -12,21 +19,47 @@ export default function Navbar() {
           <GrTechnology />
           HOSTING
         </Link>
+        <div className={styles.menu} onClick={() => setToggle(!toggle)}>
+          {toggle ? <IoCloseOutline /> : <AiOutlineMenu />}
+        </div>
       </div>
-      <ul className={styles.navLinks}>
-        <Link href="/" className={styles.navLink}>
-          Home
-        </Link>
-        <Link href="/articles" className={styles.navLink}>
-          Articles
-        </Link>
-        <Link href="/about" className={styles.navLink}>
-          About
-        </Link>
-        <Link href="/admin" className={styles.navLink}>
-          Admin Dashboard
-        </Link>
-      </ul>
+      <div
+        className={styles.navLinksWrapper}
+        style={{
+          clipPath: (toggle && "polygon(0 0%, 100% 0, 100% 100%, 0 99%)") || "",
+        }}
+      >
+        <ul className={styles.navLinks}>
+          <Link
+            href="/"
+            className={styles.navLink}
+            onClick={() => setToggle(false)}
+          >
+            Home
+          </Link>
+          <Link
+            href="/articles"
+            className={styles.navLink}
+            onClick={() => setToggle(false)}
+          >
+            Articles
+          </Link>
+          <Link
+            href="/about"
+            className={styles.navLink}
+            onClick={() => setToggle(false)}
+          >
+            About
+          </Link>
+          <Link
+            href="/admin"
+            className={styles.navLink}
+            onClick={() => setToggle(false)}
+          >
+            Admin Dashboard
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 }
